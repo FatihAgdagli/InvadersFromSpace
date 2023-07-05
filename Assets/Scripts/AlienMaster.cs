f50081b983e289952005caf93de941833ba4e878
FatihAgdagli <agdaglifatih@gmail.com>
2023-07-05 21:04:42 +0300

    Alien Wave added

--- Assets/Scripts/AlienMaster.cs
+++ Assets/Scripts/AlienMaster.cs
@@ -11,15 +11,15 @@ public class AlienMaster : MonoBehaviour
 
     [SerializeField] private GameObject bulletPrefab;
 
-    private List<GameObject> alienList = new List<GameObject>();
+    [SerializeField] private List<GameObject> alienList = new List<GameObject>();
 
     private bool movingRight;
 
     private float moveTimer = 0.01f;
     private float moveTime = 0.005f;
 
-    private Vector3 hMoveDistance = new Vector3 (0, 0, 0);
-    private Vector3 vMoveDistance = new Vector3 (0, 0, 0);
+    private Vector3 hMoveDistance = new Vector3 (0.05f, 0, 0);
+    private Vector3 vMoveDistance = new Vector3 (0, 0.15f, 0);
 
 
     private void Start()
